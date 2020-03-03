41e3204a0b126a7ebe63f3d0a7ffa0006855d1f1
RussellZhou-hub <61743623+RussellZhou-hub@users.noreply.github.com>
2020-03-03 23:43:15 +0800

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -1,6 +1,3 @@
-// Test1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <filesystem>
 #include<iomanip>
@@ -11,20 +8,20 @@
 
 using namespace std;
 
+typedef pair<int, int> my_type;     //id号，能力值
 
-class My_less {
+class My_less {                     //容器比较器
 public:
-    typedef pair<int,int> my_type;
-    static my_type sm;
-    bool operator()( my_type& t1,  my_type& t2) const {
+    static my_type sm;             //新手的id，能力值
+    bool operator()(const my_type& t1, const my_type& t2) const {
         if (abs(t1.second-sm.second) < abs(t2.second-sm.second)) return true;
         else if (abs(t1.second- sm.second) == abs(t2.second-sm.second)) {
             if (t1.second<sm.second && t2.second>sm.second) return true;
-        }
+        }                         //最接近新手的排前面
         return false;
     }
 };
-My_less::my_type sm;
+my_type My_less::sm;
 
 
 
@@ -34,31 +31,23 @@ int main()
     map<int, int>  m;
     pair<int, int> mp;
     m[1] = 1000000000;
-    sm.first = 1;
-    sm.second = 1000000000;
+    My_less ml;
+    my_type& tmp_sm=ml.sm;
+    tmp_sm.first = 1;
+    tmp_sm.second = 1000000000;
     multiset<pair<int, int>, My_less> ms;
-    ms.insert(sm);
+    ms.insert(tmp_sm);
     cin >> n;
     while (n--) {
         cin >> mp.first >> mp.second;
-        sm = mp;multiset<pair<int, int>, My_less>::iterator p;
+        tmp_sm = mp;
+        multiset<pair<int, int>, My_less>::iterator p;
         pair<int, int> p2;
         p = ms.begin();
         p2 = *p;
-        cout << p2.first << " " << p2.second << endl;
-        ms.insert(mp);
+        cout << mp.first << " "<<p2.first<<endl;
+        ms.insert(mp);                    //新手成为会员
     }
     
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
